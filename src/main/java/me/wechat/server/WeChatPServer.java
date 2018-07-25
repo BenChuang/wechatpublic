5fc99710fe20feb9be3fe141bc698957f9bd360c
ben <zhangbendren@gmail.com>
2018-07-25 23:02:53 +0800

    565

--- src/main/java/me/wechat/server/WeChatPServer.java
+++ src/main/java/me/wechat/server/WeChatPServer.java
@@ -6,31 +6,82 @@ import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.net.SocketAddress;
+import org.jdom2.CDATA;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
+import java.util.Optional;
 
 
 public class WeChatPServer {
 
-    public static void main(String[] args) {
-        test();
+    private static SAXBuilder builder = new SAXBuilder();
+
+    public static void main(String[] args) {Element respXmlRoot = new Element("xml");
+        Document respXml = new Document(respXmlRoot);
+        CDATA reToUserName = new CDATA("ToUserName");
+        reToUserName.setText("userOpid");
+        CDATA reFromUserData = new CDATA("FromUserData");
+        reFromUserData.setText("devName");
+        Element reCreateTime = new Element("CreateTime");
+        reCreateTime.setText("createTime");
+        CDATA reMsgType = new CDATA("MsgType");
+        reMsgType.setText("text");
+        CDATA reContent = new CDATA("Content");
+        reContent.setText("你好");
+        respXmlRoot
+                .addContent(reToUserName)
+                .addContent(reFromUserData)
+                .addContent(reCreateTime)
+                .addContent(reMsgType)
+                .addContent(reContent);
         Vertx vertx = Vertx.vertx();
         HttpServer server = vertx.createHttpServer();
         server.requestHandler(res -> {
             SocketAddress remoteAddress = res.connection().remoteAddress();
             System.out.println("remote address " + remoteAddress.toString());
-            res.bodyHandler(body -> {
+            res.bodyHandler((Buffer body) -> {
                 String path = res.path();
                 //区块链大数据分析页面
                 if (path.equals("/index")) {
                     res.response().setChunked(true).write("<html><head></head><body>hello world</body></html>").end();
                 } else {
                     if (body.length() > 0) {
-                        res.response().end("<xml> <ToUserName>< ![CDATA[toUser] ]></ToUserName> <FromUserName>< ![CDATA[fromUser] ]></FromUserName> <CreateTime>12345678</CreateTime> <MsgType>< ![CDATA[text] ]></MsgType> <Content>< ![CDATA[你好] ]></Content> </xml>");
+                        String rspXml = body.toString();
+                        Element root = parseToXmlRootElement(rspXml);
+                        Optional.ofNullable(root).ifPresent(r -> {
+                            String devName = r.getChildText("ToUserName");
+                            String userOpid = r.getChildText("FromUserName");
+                            String createTime = r.getChildText("CreateTime");
+                            String msgType = r.getChildText("MsgType");
+                            String content = r.getChildText("Content");
+                            String msgId = r.getChildText("MsgId");
+//                            Element respXmlRoot = new Element("xml");
+//                            Document respXml = new Document(respXmlRoot);
+//                            CDATA reToUserName = new CDATA("ToUserName");
+//                            reToUserName.setText(userOpid);
+//                            CDATA reFromUserData = new CDATA("FromUserData");
+//                            reFromUserData.setText(devName);
+//                            Element reCreateTime = new Element("CreateTime");
+//                            reCreateTime.setText(createTime);
+//                            CDATA reMsgType = new CDATA("MsgType");
+//                            reMsgType.setText("text");
+//                            CDATA reContent = new CDATA("Content");
+//                            reContent.setText("你好");
+//                            respXmlRoot
+//                                    .addContent(reToUserName)
+//                                    .addContent(reFromUserData)
+//                                    .addContent(reCreateTime)
+//                                    .addContent(reMsgType)
+//                                    .addContent(reContent);
+//                            res.response().end(respXml.toString());
+
+                        });
+                        res.response().end("<xml> <ToUserName>< ![CDATA[toUser] ]></ToUserName> <FromUserName>< ![CDATA[gh_c5a9bb72c452] ]></FromUserName> <CreateTime>12345678</CreateTime> <MsgType>< ![CDATA[text] ]></MsgType> <Content>< ![CDATA[你好] ]></Content> </xml>");
                         System.out.println("Request body: " + body.toString());
                     }
                 }
@@ -45,17 +96,15 @@ public class WeChatPServer {
         });
     }
 
-    public static void test() {
-        SAXBuilder builder = new SAXBuilder();
+    private static Element parseToXmlRootElement(String xmlStr) {
         try {
-            String xml = "<xml> <ToUserName>< ![CDATA[toUser] ]></ToUserName> <FromUserName>< ![CDATA[fromUser] ]></FromUserName> <CreateTime>12345678</CreateTime> <MsgType>< ![CDATA[text] ]></MsgType> <Content>< ![CDATA[你好] ]></Content> </xml>";
-//            xml = xml.replaceAll("<\\s!\\[CDATA\\[", "");
-//            xml = xml.replaceAll("\\]\\s\\]>", "");
-            Document document = builder.build(new ByteBufInputStream(Buffer.buffer(xml).getByteBuf()));
-            Element root = document.getRootElement();
-            Element data = root.getChild("ToUserName");
+            xmlStr = xmlStr.replaceAll("<\\s!\\[", "<![");
+            xmlStr = xmlStr.replaceAll("\\]\\s\\]>", "]]>");
+            Document document = builder.build(new StringReader(xmlStr));
+            return document.getRootElement();
         } catch (JDOMException | IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+        return null;
     }
 }
