b972a2c07b07119c87b321cc85332a1ec22e49ba
Ben <zhangbendren@gmail.com>
2018-07-27 18:48:05 +0800

    fdvfd

--- src/main/java/me/wechat/server/WeChatPServer.java
+++ src/main/java/me/wechat/server/WeChatPServer.java
@@ -35,31 +35,32 @@ public class WeChatPServer {
                         req.response().putHeader("Content-type", "text/html;charset=utf-8").end("<html><head></head><body>hello world</body></html>", "utf-8");
                     });
                 });
-            }
-            req.bodyHandler((Buffer body) -> {
-                if (body.length() > 0) {
-                    String reqXmlStr = body.toString();
-                    System.out.println("req: " + reqXmlStr);
-                    try {
-                        WechatXml reqXml = WechatXmlHelper.getWechatXml(reqXmlStr);
-                        if (reqXml.isMsgXml()) {
-                            System.out.println("resp: " + reqXml.convertToWechatMsgXml().getMsgContent());
-                            WechatMsgXml respXml = WechatXmlHelper.reqToRespMsg(reqXml);
-                            //全部编码改成utf-8
-                            respXml.setMsgContent(new String("生活就像海洋，只有意志坚强的人才能到达彼岸".getBytes(), "utf-8"));
-                            req.response().putHeader("Content-type", "text/xml;charset=utf-8").end(respXml.toString(), "utf-8");
-                        } else {
-                            System.out.println("resp: success");
-                            req.response().putHeader("Content-type", "text/plain;charset=utf-8").end("success", "utf-8");
+            } else {
+                req.bodyHandler((Buffer body) -> {
+                    if (body.length() > 0) {
+                        String reqXmlStr = body.toString();
+                        System.out.println("req: " + reqXmlStr);
+                        try {
+                            WechatXml reqXml = WechatXmlHelper.getWechatXml(reqXmlStr);
+                            if (reqXml.isMsgXml()) {
+                                System.out.println("resp: " + reqXml.convertToWechatMsgXml().getMsgContent());
+                                WechatMsgXml respXml = WechatXmlHelper.reqToRespMsg(reqXml);
+                                //全部编码改成utf-8
+                                respXml.setMsgContent(new String("生活就像海洋，只有意志坚强的人才能到达彼岸".getBytes(), "utf-8"));
+                                req.response().putHeader("Content-type", "text/xml;charset=utf-8").end(respXml.toString(), "utf-8");
+                            } else {
+                                System.out.println("resp: success");
+                                req.response().putHeader("Content-type", "text/plain;charset=utf-8").end("success", "utf-8");
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    } else {
+                        System.out.println("resp: no body");
+                        req.response().putHeader("Content-type", "text/plain;charset=utf-8").end("success", "utf-8");
                     }
-                } else {
-                    System.out.println("resp: no body");
-                    req.response().putHeader("Content-type", "text/plain;charset=utf-8").end("success", "utf-8");
-                }
-            });
+                });
+            }
         });
         server.listen(443, res -> {
             if (res.succeeded()) {
