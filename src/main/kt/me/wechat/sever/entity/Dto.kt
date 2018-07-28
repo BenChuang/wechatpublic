13ab6207bac4725ef00497bd6c50708f7274a542
Ben <zhangbendren@gmail.com>
2018-07-28 14:52:28 +0800

    fdvfd

--- src/main/kt/me/wechat/sever/entity/Dto.kt
+++ src/main/kt/me/wechat/sever/entity/Dto.kt
@@ -11,12 +11,14 @@ data class AccessToken(var accessToken: String = "",
 data class UserInfo(var openid: String = "",
                     var nickname: String = "",
                     var sex: Int = -1,
+                    var language: String = "",
                     var province: String = "",
                     var city: String = "",
                     var country: String = "",
                     var headimgurl: String = "",
                     var privilege: Array<String> = Array(0, { "" }),
                     var unionid: String = "") {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -26,6 +28,7 @@ data class UserInfo(var openid: String = "",
         if (openid != other.openid) return false
         if (nickname != other.nickname) return false
         if (sex != other.sex) return false
+        if (language != other.language) return false
         if (province != other.province) return false
         if (city != other.city) return false
         if (country != other.country) return false
@@ -40,6 +43,7 @@ data class UserInfo(var openid: String = "",
         var result = openid.hashCode()
         result = 31 * result + nickname.hashCode()
         result = 31 * result + sex
+        result = 31 * result + language.hashCode()
         result = 31 * result + province.hashCode()
         result = 31 * result + city.hashCode()
         result = 31 * result + country.hashCode()
@@ -48,4 +52,5 @@ data class UserInfo(var openid: String = "",
         result = 31 * result + unionid.hashCode()
         return result
     }
+
 }
